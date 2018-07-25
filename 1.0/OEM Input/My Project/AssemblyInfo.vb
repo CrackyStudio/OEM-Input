0eef47bcc94050769f8f8a4fa15360e5dad1f125
Antoine Nivoy <antoine.nivoy@gmail.com>
2018-07-25 12:09:57 +0200

    Add files via upload

---
+++ 1.0/OEM Input/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Vérifiez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("OEM Input")>
+<Assembly: AssemblyDescription("Customize OEM Informations")>
+<Assembly: AssemblyCompany("Cracky Studio")>
+<Assembly: AssemblyProduct("OEM Input")>
+<Assembly: AssemblyCopyright("GNU General Public License v3.0")>
+<Assembly: AssemblyTrademark("Cracky Studio")>
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("5d6a20f7-5a3b-4949-a0cb-63c8ddd04937")>
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
